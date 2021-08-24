8a2cbfe63b1bf31da227c77abf3be51950691c30
yunchaoq <yunchaoq@github.com>
2021-08-24 20:57:42 +0800

    * init

---
+++ heao.gov.cn-search.js
@@ -0,0 +1,256 @@
+// ==UserScript==
+// @name         河南省2021年普通高等学校招生考试成绩查询（含对口生和专升本） 油猴脚本
+// @namespace    https://github.com/mengyou658/heao.gov.cn-search
+// @version      1.0.0
+// @description  河南省2021年普通高等学校招生考试成绩查询（含对口生和专升本） 油猴脚本
+// @author       yunchaoq/mengyou658
+// @license      GPL License
+// @match        *://*.heao.gov.cn/*
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// @require      https://cdn.bootcdn.net/ajax/libs/layer/3.1.1/layer.min.js
+// @resource     layerCss https://cdn.bootcdn.net/ajax/libs/layer/3.1.1/theme/default/layer.min.css
+// @grant        GM_addStyle
+// @grant        GM_setClipboard
+// @grant        GM_getResourceText
+// ==/UserScript==
+(function () {
+  'use strict';
+  GM_addStyle(GM_getResourceText('layerCss'))
+  // GM_addStyle(GM_getResourceText('layuiCss'))
+  var location = window.location.href;
+  // 首页
+  let homePageFlag = location.indexOf("http://www.heao.gov.cn/HEAODataCenter/PagePZQuery") > -1 ;
+  if (homePageFlag) {
+    init();
+  }
+
+  // insertScript('https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js')
+  function init() {
+
+    if (homePageFlag) {
+      // 首页
+      homePageInit();
+    }
+
+  }
+
+  function homePageInit() {
+    showMsg('查询页面')
+    // 首页
+    $('#bmxhradio').attr("checked", true);
+    showModal()
+  }
+
+  function showModal() {
+    $('#InputCheck').prop('autocomplete', 'off')
+    var tmp = getInitData()
+    var data = tmp.data
+    var dataState = tmp.dataState
+    layer.confirm('当前查询数据: ' + (data && dataState && dataState.index < data.length ? data[dataState.index]['姓名'] : ''), {
+      btn: ['从头开始', '上一个', '下一个', '继续当前', '保存当前', '下载结果'] //可以无限个按钮
+          ,closeBtn: 0, shade: 0, offset: 'rt'
+      ,btn1: function(index1, layero){
+        //从头开始
+        var tmp = getInitData()
+        var data = tmp.data
+        var dataState = tmp.dataState
+        if (!data || !data.length) {
+          showMsg('等待获取数据，请设置localStorage.setItem("tmpSearchCsvData",[{"考生号":"xxx","报名序号":"xxx","班级":"xxx","姓名":"xxxx","语文":"","数学":"","外语":"","综合":"","总分":"0"},])')
+          return;
+        }
+        dataState = {
+          index: 0,
+          searchCount: 0,
+          state: 1
+        }
+        localStorage.setItem("tmpSearchCsvDataState", JSON.stringify(dataState));
+        startSearch(data[dataState.index])
+        showModal()
+      },btn2: function(index1, layero){
+        //上一个
+        var tmp = getInitData()
+        var data = tmp.data
+        var dataState = tmp.dataState
+        if (!data || !data.length) {
+          showMsg('等待获取数据，请设置localStorage.setItem("tmpSearchCsvData",[{"考生号":"xxx","报名序号":"xxx","班级":"xxx","姓名":"xxxx","语文":"","数学":"","外语":"","综合":"","总分":"0"},])')
+          return;
+        }
+        saveData()
+        if (dataState && dataState.index <= 1) {
+          showMsg("当前是第一个数据")
+          return;
+        }
+        dataState.index -= 1
+        localStorage.setItem("tmpSearchCsvDataState", JSON.stringify(dataState));
+        startSearch(data[dataState.index])
+        showModal()
+      },btn3: function(index1, layero){
+        //下一个
+        var tmp = getInitData()
+        var data = tmp.data
+        var dataState = tmp.dataState
+        if (!data || !data.length) {
+          showMsg('等待获取数据，请设置localStorage.setItem("tmpSearchCsvData",[{"考生号":"xxx","报名序号":"xxx","班级":"xxx","姓名":"xxxx","语文":"","数学":"","外语":"","综合":"","总分":"0"},])')
+          return;
+        }
+        saveData()
+        if (dataState && dataState.index >= data.length - 1) {
+          showMsg("当前是最后一个数据，")
+          return;
+        }
+        dataState.index += 1
+        localStorage.setItem("tmpSearchCsvDataState", JSON.stringify(dataState));
+        startSearch(data[dataState.index])
+        showModal()
+      }
+      ,btn4: function(index1, layero){
+        //继续当前
+        var tmp = getInitData()
+        var data = tmp.data
+        var dataState = tmp.dataState
+        if (!data || !data.length) {
+          showMsg('等待获取数据，请设置localStorage.setItem("tmpSearchCsvData",[{"考生号":"xxx","报名序号":"xxx","班级":"xxx","姓名":"xxxx","语文":"","数学":"","外语":"","综合":"","总分":"0"},])')
+          return;
+        }
+        if (dataState && dataState.index > data.length - 1) {
+          showMsg("当前是最后一个数据")
+          return;
+        }
+        startSearch(data[dataState.index])
+        showModal()
+      }
+      ,btn5: function(index1, layero){
+        //保存当前
+        saveData()
+        showModal()
+      }
+      ,btn6: function(index1, layero){
+        //下载数据
+        download()
+        showModal()
+      }
+    })
+  }
+
+  function saveData() {
+    var tabInfo = $('#tabInfo')
+    if (tabInfo && tabInfo.length) {
+      var res = []
+      tabInfo.find('td:odd').each(function () {
+        res.push($(this).text())
+      });
+
+      var tmp = getInitData()
+      var data = tmp.data
+      var dataState = tmp.dataState
+      if (data && dataState && dataState.index < data.length) {
+        data[dataState.index].scores = res
+        localStorage.setItem("tmpSearchCsvData", JSON.stringify(data));
+        showMsg("保存成功")
+      }
+    } else {
+      showMsg("当前没有查询到数据，无法保存")
+    }
+  }
+
+  function getInitData() {
+    var data = localStorage.getItem("tmpSearchCsvData")
+    if (data) {
+      try {
+        data = JSON.parse(data)
+      } catch (e) {
+        data = null
+      }
+    }
+    var dataState = localStorage.getItem("tmpSearchCsvDataState")
+    if (dataState) {
+      try {
+        dataState = JSON.parse(dataState)
+      } catch (e) {
+        dataState = null
+      }
+    }
+    return {data, dataState}
+  }
+
+  function startSearch(item) {
+    $('#bmxhradio').click()
+    $('#bmxhradio').attr('checked', true);
+    if (!item['考生号'] || !item['报名序号']) {
+      var dataState = localStorage.getItem("tmpSearchCsvDataState")
+      dataState.index += 1
+      dataState.state = 0
+      localStorage.setItem("tmpSearchCsvDataState", JSON.stringify(dataState))
+      return
+    }
+    showMsg("当前查询数据："+item['姓名']+"，请输入验证码")
+    $('#ksh').val(item['考生号']);
+    $('#ksh').prop('autocomplete', 'off')
+    $('#sfzh').val(item['报名序号']);
+    $('#sfzh').prop('autocomplete', 'off')
+    $('#CheckPic').click();
+  }
+
+  function download() {
+    var data = localStorage.getItem("tmpSearchCsvData")
+    if (data) {
+      try {
+        data = JSON.parse(data)
+      } catch (e) {
+        data = null
+      }
+    }
+    if (!data) {
+      showMsg("当前没有数据，无法下载");
+      return
+    }
+    var res = ['考生号,报名序号,班级,姓名,语文,数学,外语,综合,总分']
+    for (var i in data) {
+      var item = data[i]
+      var tmp = []
+      if (!item['考生号']) {
+        continue
+      }
+      tmp.push(item['考生号']);
+      tmp.push(item['报名序号'])
+      tmp.push(item['班级'])
+      tmp.push(item['姓名'])
+      if (item.scores) {
+        tmp.push(item.scores[4])
+        tmp.push(item.scores[5])
+        tmp.push(item.scores[6])
+        tmp.push(item.scores[7])
+        tmp.push(item.scores[9])
+      }
+      res.push(tmp.join(','))
+    }
+    console.log('currentData download', res)
+    downloadCsv(res.join('\r\n'));
+  }
+
+  function showMsg(msg, callback) {
+    console.log(msg)
+    layer.msg(msg, callback)
+  }
+
+  function downloadCsv(data) {
+    var str = data;
+    var blob = new Blob([str], {type: '.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel'});
+    const url3 = window.URL.createObjectURL(blob);
+
+    var filename = '高考查询结果-' + new Date().getTime() + '.csv';
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = url3;
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  function insertScript(src) {
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = src;
+    document.body.appendChild(newScript);
+  }
+})();
